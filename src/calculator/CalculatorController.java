5c058d256bd521ee7146b4efc509805342e84e4d
Christopher Dierolf <chidi117@gmail.com>
2019-09-09 15:47:28 -0500

    first commit

---
+++ src/calculator/CalculatorController.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculator;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+/**
+ *
+ * @author pis7ftw
+ */
+public class CalculatorController implements Initializable {
+    
+    @FXML private Button cos;
+    @FXML private Button sin;
+    @FXML private Button tan;
+    @FXML private Button backSpace;
+    @FXML private Button clear;
+    @FXML private Button sqrt;
+    @FXML private Button xToYPower;
+    @FXML private Button xSquared;
+    @FXML private Button xCubed;
+    @FXML private Button ln;
+    @FXML private Button log;
+    @FXML private Button oneOverX;
+    @FXML private Button absolute;
+    @FXML private Button pi;
+    @FXML private Button eulers;
+    @FXML private Button openParenthesis;
+    @FXML private Button closeParenthesis;
+    @FXML private Button percent;
+    @FXML private Button frac;
+    @FXML private Button divide;
+    @FXML private Button seven;
+    @FXML private Button eight;
+    @FXML private Button nine;
+    @FXML private Button multiply;
+    @FXML private Button four;
+    @FXML private Button five;
+    @FXML private Button six;
+    @FXML private Button minus;
+    @FXML private Button three;
+    @FXML private Button two;
+    @FXML private Button one;
+    @FXML private Button plus;
+    @FXML private Button negative;
+    @FXML private Button zero;
+    @FXML private Button decimal;
+    @FXML private Button equals;
+    
+    
+    
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
---
+++ src/calculator/classes/CalculatorClass.java
@@ -0,0 +1,13 @@
+package calculator.classes;
+
+/**
+ * @Author Name: Christopher K. Dierolf
+ * @Date: Sep 9, 2019
+ * @Subclass CalculatorClass Description: 
+ */
+//Imports
+
+//Begin Subclass CalculatorClass
+public class CalculatorClass {
+
+} //End Subclass CalculatorClass
